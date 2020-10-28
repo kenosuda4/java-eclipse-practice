28d9b0e5f9192fe2bc1c3743a4c063016975e6d5
kenosuda4 <rs7ten@gmail.com>
2020-10-29 05:15:17 +0900

    make prc-2

---
+++ practice2/src/practice2/prc2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package practice2;
+
+/**
+ * @author shimizuryuunosuke
+ *
+ */
+public class prc2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
