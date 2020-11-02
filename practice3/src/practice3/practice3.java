a5f710e7725c7183db599b70027838775d707483
kenosuda4 <rs7ten@gmail.com>
2020-11-02 17:46:21 +0900

    prc if

--- practice3/src/practice3/practice3.java
+++ practice3/src/practice3/practice3.java
@@ -41,22 +41,56 @@ public class practice3 {
 	    System.out.println(5 > 2);
 
 	    //	理論演算詞
-	    // trueと出力されるようにしてください
+	    System.out.println("理論演算詞");
+	    // trueと出力される(どちらか)
 	    System.out.println(true || false);
 
-	    // falseと出力されるようにしてください
+	    // falseと出力される (尚且つ)
 	    System.out.println(false && true);
 
-	    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+	    // 「8 < 5」かつ「3 >= 2」の結果を出力 (尚且つ 両方満たすとtrue) 結果 false
 	    System.out.println(8 < 5 && 3 >= 2 );
 
-	    // 「8 < 5」または「3 >= 2」の結果を出力してください
+	    // 「8 < 5」または「3 >= 2」の結果を出力 (どちらかを満たすとtrue) 結果 true
 	    System.out.println(8 < 5 || 3 >= 2);
 
-	    // 「8 < 5」でない、の結果を出力してください
+	    // 「8 < 5」でない、の結果を出力 !は条件反転 結果true
 	    System.out.println(!(8 < 5));
 
+	    // 条件分岐
+	    System.out.println("条件分岐");
+	    // 条件式に直接trueをいれてください
+	    if (true) {
+	      System.out.println("条件式がtrueのため、出力されます");
+	    }
+
+	    // 条件式に直接falseをいれてください
+	    if (false) {
+	      System.out.println("条件式がfalseのため、出力されません");
+	    }
+
+	    int x = 5;
+	    // if文を用いて、変数xが3より大きいとき「xは3より大きい」と出力
+	    if (x > 3){
+	      System.out.println("xは2より大きい");
+	    }
+
+	    // if文を用いて、変数xが8より大きいとき「xは9より大きい」と出力してください
+	    if (x > 9){
+	      System.out.println("xは9より大きい");
+
 		}
+
+	    // xが20より小さいとき、どちらでもないときの条件分岐を追加してください
+	    if (x < 10){
+	      System.out.println("10以下");
+	    }else if(x < 20){
+	      System.out.println("10以上、20より小さい");
+	    }else{
+	      System.out.println("20以上");
+	    }
+
+	}
 }
 
 
